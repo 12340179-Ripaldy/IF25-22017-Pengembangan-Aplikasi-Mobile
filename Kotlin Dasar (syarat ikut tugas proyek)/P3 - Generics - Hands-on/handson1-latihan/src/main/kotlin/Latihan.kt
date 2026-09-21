18ebd8ece66d3b9e94996ce2de2ee0e8a058f3ed
Ripady Saputra Lumbantoruan <ripaldy.123140179@student.itera.ac.id>
2026-09-21 16:31:46 +0700

    Handson-Done

--- Kotlin Dasar (syarat ikut tugas proyek)/P3 - Generics - Hands-on/handson1-latihan/src/main/kotlin/Latihan.kt
+++ Kotlin Dasar (syarat ikut tugas proyek)/P3 - Generics - Hands-on/handson1-latihan/src/main/kotlin/Latihan.kt
@@ -4,9 +4,10 @@
 // menggunakan fungsi transformasi, tanpa mengubah Box aslinya.
 
 class Box<T>(val value: T) {
-    // TODO 1: Tambahkan fungsi generik map<R>(transform: (T) -> R): Box<R>
-    // yang mengembalikan Box<R> baru berisi hasil transform(value)
-
+   // TODO 1: Menambahkan fungsi map<R> yang mereturn Box<R> baru
+    fun <R> map(transform: (T) -> R): Box<R> {
+        return Box(transform(value))
+    }
 }
 
 fun main() {
@@ -16,13 +17,17 @@ fun main() {
     val cupBox = Box("cup") // Tipe parameter bisa di-infer
     println("cupBox.value = ${cupBox.value}")
 
-    // TODO 2: Gunakan map untuk mengubah intBox (Box<Int>) menjadi Box<String>
-    // berisi representasi string dari angkanya, misal "Angka: 23"
-    // val stringBox = intBox.map { ... }
-    // println("stringBox.value = ${stringBox.value}")
-
-    // TODO 3: Gunakan map untuk mengubah cupBox (Box<String>) menjadi Box<Int>
-    // berisi panjang string-nya
-    // val lengthBox = cupBox.map { ... }
-    // println("lengthBox.value = ${lengthBox.value}")
+    // TODO 2: Menggunakan map (mengubah 23 menjadi tulisan "Angka: 23")
+    val stringBox = intBox.map { "Angka: $it" }
+    println("stringBox.value = ${stringBox.value}")
+    
+    // TODO 3: Mengubah tulisan "cup" menjadi angka jumlah hurufnya (3)
+    val lengthBox = cupBox.map { it.length }
+    println("lengthBox.value = ${lengthBox.value}")
 }
+
+// Output:
+// intBox.value = 23
+// cupBox.value = cup
+// stringBox.value = Angka: 23
+// lengthBox.value = 3
